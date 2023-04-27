34fe9111792b470b36348ece2d720302effd4b5b
sean2474 <sean24740@gmail.com>
2023-04-27 16:26:35 -0400

    small change

--- front/lib/auth/login.dart
+++ front/lib/auth/login.dart
@@ -4,7 +4,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../storage/local_storage.dart';
-import 'package:front/widgets/assets.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:front/api_service/authenication.dart';
